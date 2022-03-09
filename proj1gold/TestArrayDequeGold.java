c933afc8a924a9e8ec47b45e6f191312acc927a5
daybleed21 <shilin21@hust.edu.cn>
2022-03-09 10:43:29 +0800

    fix error

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -5,45 +5,53 @@ public class TestArrayDequeGold {
 
     @Test
     public void testSomeMethod(){
+        StringBuilder msg = new StringBuilder("\n");
         StudentArrayDeque<Integer> student = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> array = new ArrayDequeSolution<>();
         int count = 0;
         for(int i = 0; i < 500; i++){
             if(i % 5 == 0){
-                assertEquals("Test size()",student.size(),array.size());
+                msg.append("size()\n");
+                assertEquals(msg.toString(),student.size(),array.size());
             }
             double num = StdRandom.uniform();
             if(num < 0.25){
                 student.addLast(i);
                 array.addLast(i);
+                msg.append("addLast("+ i +")\n");
 //                assertEquals("StudentArrayDeque addLast(" + i + ")",(Integer) i,student.get(count));
                 count++;
-                assertEquals("addLast("+i+")", array.get(count - 1), student.get(count - 1));
+                assertEquals(msg.toString(), array.get(count - 1), student.get(count - 1));
             }else if( num < 0.5) {
                 student.addFirst(i);
                 array.addFirst(i);
                 count++;
+                msg.append("addFirst(" + i + ")\n");
 //                assertEquals("StudentArrayDeque addFirst (" + i +")",(Integer)i,student.get(0));
-                assertEquals("addFirst(" + i + ")", array.get(0), student.get(0));
+                assertEquals(msg.toString(), array.get(0), student.get(0));
             }else if( num < 0.75 ){
                 if(student.isEmpty()){
+                    msg.append("isEmpty()\n");
                     assertTrue("isEmpty()", student.isEmpty());
                     continue;
                 }
                 Integer stuActual = student.removeFirst();
                 Integer arrActual = array.removeFirst();
-                assertEquals("removeFirst()",arrActual,stuActual);
+                msg.append("removeFirst()\n");
+                assertEquals(msg.toString(),arrActual,stuActual);
                 count--;
             }else {
                 if(student.isEmpty()){
-                    assertTrue("isEmpty()", student.isEmpty());
+                    msg.append("isEmpty()\n");
+                    assertTrue(msg.toString(), student.isEmpty());
                     continue;
                 }
                 Integer stuActual = student.removeLast();
                 Integer arrActual = array.removeLast();
-                assertEquals("removeLast()",arrActual,stuActual);
-                //assertEquals("ArrayDequeSolution removeLast()",arrExpected,arrActual);
                 count--;
+                msg.append("removeLast()\n");
+                assertEquals(msg.toString(),arrActual,stuActual);
+                //assertEquals("ArrayDequeSolution removeLast()",arrExpected,arrActual);
             }
 
 
